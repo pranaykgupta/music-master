db533a8a8fc450939866ba4fdb4158ffe8abbfd6
Pranay <pranaykgupta21@gmail.com>
2021-06-16 11:36:52 +0530

    fetched data from api and added search ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
 
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
+
 class App extends Component {
+    state = { artistQuery: '', artist: null, tracks: []};
+
+    updateArtistQuery = (event) => {
+        // console.log('event.target.value', event.target.value);
+        this.setState({artistQuery: event.target.value});
+    }
+
+    searchArtist = () => {
+        fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+            .then(response => response.json())
+            .then(json => {
+                // console.log(json.artists.total);
+                if(json.artists.total > 0){
+                    const artist = json.artists.items[0];
+                    this.setState({artist});
+
+                    fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+                        .then(response => response.json())
+                        .then(json => this.setState({tracks: json.tracks}))
+                        .catch(error => alert(error.message));
+                }
+            })
+            .catch(error => alert(error.message));
+    }
+
+    handleKeyPress = (event) => {
+        if(event.key === 'Enter'){
+            this.searchArtist();
+        }
+    }
+
     render() {
+        console.log('this.state', this.state);
         return(
             <div>
-                React App
+               <h2>Music Master</h2>
+               <input 
+                    onChange={this.updateArtistQuery}
+                    placeholder='Search for an artist'
+                    onKeyPress={this.handleKeyPress}
+               />
+               <button onClick={this.searchArtist}>Search</button>
             </div>
         );
     }
