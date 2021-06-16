c9dc348f08b603e46967559bf77d9349ee607f10
Pranay <pranaykgupta21@gmail.com>
2021-06-16 20:44:15 +0530

    completed the music master project

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import Artist from './Artist';
+import Search from './Search';
 import Tracks from './Tracks';
 
 const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
 
 class App extends Component {
-    state = { artistQuery: '', artist: null, tracks: []};
+    state = { artist: null, tracks: []};
 
-    updateArtistQuery = (event) => {
-        // console.log('event.target.value', event.target.value);
-        this.setState({artistQuery: event.target.value});
+    componentDidMount(){
+        this.searchArtist('Honey Singh');
     }
 
-    searchArtist = () => {
-        fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+    searchArtist = (artistQuery) => {
+        fetch(`${API_ADDRESS}/artist/${artistQuery}`)
             .then(response => response.json())
             .then(json => {
                 // console.log(json.artists.total);
@@ -30,23 +30,12 @@ class App extends Component {
             .catch(error => alert(error.message));
     }
 
-    handleKeyPress = (event) => {
-        if(event.key === 'Enter'){
-            this.searchArtist();
-        }
-    }
-
     render() {
         // console.log('this.state', this.state);
         return(
             <div>
                <h2>Music Master</h2>
-               <input 
-                    onChange={this.updateArtistQuery}
-                    placeholder='Search for an artist'
-                    onKeyPress={this.handleKeyPress}
-               />
-               <button onClick={this.searchArtist}>Search</button>
+               <Search searchArtist={this.searchArtist} />
                <Artist artist={this.state.artist} />
                <Tracks tracks={this.state.tracks} />
             </div>
--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -21,23 +21,43 @@ class Tracks extends Component{
 
     }
 
+    trackIcon = track => {
+        if(!track.preview_url){
+            return <span>N/A</span>;
+        }
+        if(
+            this.state.playing &&
+            this.state.playingPreviewUrl === track.preview_url
+        ){
+            return <span>| |</span>;
+        }
+        return <span>&#9654;</span>;
+    }
+
     render(){
         const { tracks } = this.props;
         return(
             tracks.map( track => {
                 const {id, name, album, preview_url} = track;
                 return(
-                    <div key={id}>
+                    <div 
+                        key={id} 
+                        onClick={this.playAudio(preview_url)} 
+                        className="track"
+                    >
                         <img 
                             src={album.images[0].url}
                             alt="track"
-                            onClick={this.playAudio(preview_url)}
+                            className="track-image"
+                            
                         />
-                        <p>{name}</p>
+                        <p className='track-text'>{name}</p>
+                        <p className='track-icon'>{this.trackIcon(track)}</p>
                     </div>
                 )
                 
             })
+
         )
     }
 }
